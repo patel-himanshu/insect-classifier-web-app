53fb4c26522a1773417c993ed90a9969d3f5b8f1
Himanshu Patel <hpatel27299@gmail.com>
2020-06-14 03:01:09 +0530

    Preparing prediction function

--- Insect_Classifier/urls.py
+++ Insect_Classifier/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from classifier import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    url('predictImage', views.predictImage, name='predictImage'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- classifier/views.py
+++ classifier/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+import tensorflow
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import Image
+import json
+
+# Image Preprocessing, Reading Labels, and Loading model
+# img_height, img_width = 224, 224
+with open('./models/labels.json', 'r') as labelFile:
+    labels = labelFile.read()
+
+labels = json.loads(labels)
+
+model = load_model('./models/googlenet.h5')
+
 def index(request):
     context = {'a': 1}
+    return render(request, 'classifier/index.html', context)
+
+def predictImage(request):
+    fileObj = request.FILES['filePath']
+    fs = FileSystemStorage()
+    filePathName = fs.save(fileObj.name, fileObj)
+    filePathName = fs.url(filePathName)
+
+    img = Image.load_img(test, target_size=(224, 224))
+    x = image
+    context = {'filePathName': filePathName}
     return render(request, 'classifier/index.html', context)
